37e6a8713669a1193c52f7f3b1c30c2bf4512fcb
Klaus Wehner <74401112+KlausWehner@users.noreply.github.com>
2021-07-17 14:26:42 +0200

    update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const Director = Models.Director
 
-mongoose.connect('mongodb://localhost:27017/flixMoviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/flixMoviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect('mongodb+srv://myFlixDBadmin:Password01@myflixdb.8gcoi.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://myFlixDBadmin:Password01@myflixdb.8gcoi.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
 app = express();
